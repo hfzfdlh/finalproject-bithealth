c5035b4435f961bd98f078c2fc26c451df319e48
hfzfdlh <fadillahhafiz.fadillah@gmail.com>
2024-01-18 23:50:26 +0700

    feat: add navigation and todos template

---
+++ js/navigation.js
@@ -0,0 +1,45 @@
+const objectNav = {
+    "home-container":true,
+    "contact-container": false,
+    "about-container" : false,
+    "portofolio-container":false,
+    "blog-container" : false,
+    "todos-container" : false,
+  }
+
+  const homeNav = document.getElementById("home-nav")
+  const contactNav = document.getElementById("contact-nav")
+  const aboutNav = document.getElementById("about-nav")
+  const portofolioNav = document.getElementById("portofolio-nav")
+  const blogNav = document.getElementById("blog-nav")
+  const todosNav = document.getElementById("todos-nav")
+
+  homeNav.addEventListener("click", ()=>{
+    event.preventDefault()
+    switchOn("home-container")
+  })
+
+  contactNav.addEventListener("click", ()=>{
+    event.preventDefault()
+    switchOn("contact-container")
+  })
+
+  aboutNav.addEventListener("click", ()=>{
+    event.preventDefault()
+    switchOn("about-container")
+  })
+
+  portofolioNav.addEventListener("click", ()=>{
+    event.preventDefault()
+    switchOn("portofolio-container")
+  })
+
+  blogNav.addEventListener("click", ()=>{
+    event.preventDefault()
+    switchOn("blog-container")
+  })
+
+  todosNav.addEventListener("click", ()=>{
+    event.preventDefault()
+    switchOn("todos-container")
+  })
